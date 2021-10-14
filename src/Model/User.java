be159e431d9b67fbf40e310669d9c2f035e25c07
Diego Segovia <segoviadiegomatias@gmail.com>
2021-10-13 21:15:39 -0300

    chore(Setup Project): Project Scaffolding

---
+++ src/Model/User.java
@@ -0,0 +1,84 @@
+package Model;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String dni;
+    private String createdAt;
+    private String username;
+    private String password;
+
+    public User(String firstName, String lastName, String address, String dni, String createdAt, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.dni = dni;
+        this.createdAt = createdAt;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
