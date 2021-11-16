b4f999d50dc6383ea490bf50e14cf675d710233d
Agustin Buset <agustin.buset@gmail.com>
2021-11-15 23:12:16 -0300

    - creada la vista del formulario de turnos

---
+++ src/com/company/UI/CreateAppointment.java
@@ -0,0 +1,21 @@
+package com.company.UI;
+
+import javax.swing.*;
+
+public class CreateAppointment {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox3;
+    private JLabel selectDoctorLabel;
+    private JLabel selectDateLabel;
+    private JLabel selectTimeLabel;
+    private JButton confirmAppointmentButton;
+    private JButton cancelButton;
+    private JComboBox comboBox2;
+    private JComboBox comboBox4;
+    private JComboBox comboBox5;
+    private JLabel añoLabel;
+    private JLabel mesLabel;
+    private JLabel diaLabel;
+
+}
